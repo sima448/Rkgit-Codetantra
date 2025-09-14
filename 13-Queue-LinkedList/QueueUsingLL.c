4aae571cd799786b69fd2f9bf7154173a294ee9e
Sima Kumari <simak4484@gmail.com>
2025-09-14 20:28:12 +0530

    Create QueueUsingLL.c

---
+++ 13-Queue-LinkedList/QueueUsingLL.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "QueueOperationsLL.c"
+int main() {
+	int op, x;
+	while(1) {	
+		printf("1.Enqueue 2.Dequeue 3.Display 4.Is Empty 5.Size 6.Exit\n");
+		printf("Enter your option : ");
+		scanf("%d",&op);
+		switch(op) {
+			case 1: 
+				printf("Enter element : ");
+				scanf("%d",&x);
+				enqueue(x);
+				break;
+			case 2: 
+				dequeue();
+				break;
+			case 3: 
+				display();
+				break;
+			case 4:
+				isEmpty();
+				break;
+			case 5:
+				size();
+				break;
+			case 6: exit(0);
+		}
+	}
+}
